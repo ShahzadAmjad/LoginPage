8a1774713da5365d56d06941eca5418d83ab0a11
Administrator <shahzadamjad94@gmail.com>
2021-11-11 16:22:57 +0500

    update

--- LoginPage/Models/Login/Login.cs
+++ LoginPage/Models/Login/Login.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -8,6 +9,8 @@ namespace LoginPage.Models.Login
     public class Login
 
     {
+        [Key]
+        public int id { get; set; }
         public string Email { get; set; }
         public string Password { get; set; }
     }
